c67247efe30d3945efda5f9e1bdfa6a4540d625c
Samy HENDEL <hendels@w.ensma.fr>
2022-01-20 11:40:51 +0100

    removing all the boilerplate

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,18 +1,22 @@
-import { fetch } from "as-fetch";
+// import { fetch } from "as-fetch";
 
-export async function getData(): string {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users?_limit=3", {
-    method: "GET",
-    mode: "no-cors",
-    headers: [],
-    body: null
-  })
-  const data = await response.json()
-  return data
-}
+// export async function getData(): string {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users?_limit=3", {
+//     method: "GET",
+//     mode: "no-cors",
+//     headers: [],
+//     body: null
+//   })
+//   const data = await response.json()
+//   return data
+// }
 
 export function getNum(): i32{
   return 3
+}
+
+export function getGivenNum(num: i32): i32{
+  return num
 }
 
 export function getStr(): string {
--- server/public/content.js
+++ server/public/content.js
@@ -2,10 +2,11 @@ loader.instantiate(
     // Binary to instantiate
     fetch("http://localhost:3000/fetch.wasm")
   ).then((wasm) => {
-    const {getNum, getStr, makeStr, getData, __newString, __getString} = wasm.exports;
+    const {getNum, getStr, makeStr, getGivenNu, __newString, __getString} = wasm.exports;
       
       console.log(getNum())
+      console.log(getGivenNu(18))
       console.log(__getString(getStr()));
       console.log(__getString(makeStr(__newString("Hello, there :)"))));
-      console.log(__getString(getData()));
+      // console.log(__getString(getData()));
   })
